6873792e70d0b5737bb9a79712be60c4e41d3ead
Denis Ivanchenko <sicknature24@yandex.com>
2024-07-02 21:34:17 +0300

    feat: create project components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,9 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { CreateItemComponent } from './admin/create-item/create-item.component';
+import { MainComponent } from './core/main/main.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { FilterComponent } from './core/header/filter/filter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    LoginComponent,
+    RegistrationComponent,
+    CreateItemComponent,
+    MainComponent,
+    PageNotFoundComponent,
+    FilterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
