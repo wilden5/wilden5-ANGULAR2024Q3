1d6142f474fd4dd814b23721a0dd666dfa0c1bb0
Denis Ivanchenko <sicknature24@yandex.com>
2024-07-04 11:21:20 +0300

    feat: extend custom button component with height input

--- youtube-client-app/src/app/shared/components/custom-button/custom-button.component.ts
+++ youtube-client-app/src/app/shared/components/custom-button/custom-button.component.ts
@@ -1,14 +1,17 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-custom-button',
   templateUrl: './custom-button.component.html',
   styleUrls: ['./custom-button.component.scss'],
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, NgStyle],
 })
 export class CustomButtonComponent {
+  @Input() buttonHeightREM?: number;
+
   @Output() buttonClickEvent = new EventEmitter<void>();
 
   buttonClick(): void {
