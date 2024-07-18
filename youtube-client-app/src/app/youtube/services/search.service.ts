b46303784cb338af36a519d40e4ea6ee0a9d300d
Denis Ivanchenko <sicknature24@yandex.com>
2024-07-18 13:18:35 +0300

    fix: move filter related methods from search service to filter service

--- youtube-client-app/src/app/youtube/services/search.service.ts
+++ youtube-client-app/src/app/youtube/services/search.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, concatMap, map, Observable, of, tap, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { SearchItem } from '../models/search-item';
-import { FilterByKeywordPipe } from '../../shared/pipes/filter-by-keyword.pipe';
 import { SearchResponse } from '../models/search-response';
 
 @Injectable({
@@ -13,16 +12,9 @@ export class SearchService {
 
   filteredSearchItems: Observable<SearchItem[]> = of([]);
 
-  searchByDateAscending = false;
-
-  searchByViewsAscending = false;
-
   searchQuery: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
-  constructor(
-    private http: HttpClient,
-    private filterByKeywordPipe: FilterByKeywordPipe
-  ) {}
+  constructor(private http: HttpClient) {}
 
   performSearchByValue(value: string): Observable<SearchItem[]> {
     return this.http.get<SearchResponse>(`search?maxResults=20&q=${value}`).pipe(
@@ -56,38 +48,6 @@ export class SearchService {
         return throwError(() => error);
       })
     );
-  }
-
-  performSortByDate(): Observable<SearchItem[]> {
-    this.searchByDateAscending = !this.searchByDateAscending;
-    if (this.searchByDateAscending) {
-      return (this.filteredSearchItems = this.filteredSearchItems.pipe(
-        map((result) =>
-          result.sort((a, b) => new Date(a.snippet.publishedAt).getTime() - new Date(b.snippet.publishedAt).getTime())
-        )
-      ));
-    }
-    return (this.filteredSearchItems = this.filteredSearchItems.pipe(
-      map((result) =>
-        result.sort((a, b) => new Date(b.snippet.publishedAt).getTime() - new Date(a.snippet.publishedAt).getTime())
-      )
-    ));
-  }
-
-  performSortByViews(): Observable<SearchItem[]> {
-    this.searchByViewsAscending = !this.searchByViewsAscending;
-    if (this.searchByViewsAscending) {
-      return (this.filteredSearchItems = this.filteredSearchItems.pipe(
-        map((result) => result.sort((a, b) => Number(a.statistics.viewCount) - Number(b.statistics.viewCount)))
-      ));
-    }
-    return (this.filteredSearchItems = this.filteredSearchItems.pipe(
-      map((result) => result.sort((a, b) => Number(b.statistics.viewCount) - Number(a.statistics.viewCount)))
-    ));
-  }
-
-  performFilterByKeyword(searchQuery: string): Observable<SearchItem[]> {
-    return (this.filteredSearchItems = this.filterByKeywordPipe.transform(this.searchItems, searchQuery));
   }
 
   // currently is not in use
