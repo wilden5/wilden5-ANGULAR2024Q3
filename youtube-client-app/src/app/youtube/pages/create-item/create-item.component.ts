5b536d8392c8d5492ebe50ca41076f5f050f4b3c
Denis Ivanchenko <sicknature24@yandex.com>
2024-07-22 13:47:03 +0300

    feat: add validation rules for create-item component

--- youtube-client-app/src/app/youtube/pages/create-item/create-item.component.ts
+++ youtube-client-app/src/app/youtube/pages/create-item/create-item.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { dateValidator } from '../../validators/date.validator';
 
 @Component({
   selector: 'app-create-item',
@@ -8,11 +9,11 @@ import { FormArray, FormBuilder, Validators } from '@angular/forms';
 })
 export class CreateItemComponent {
   createItemForm = this.fb.group({
-    title: ['', [Validators.required]],
-    description: ['', [Validators.required]],
+    title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+    description: ['', [Validators.maxLength(255)]],
     imageLink: ['', [Validators.required]],
     videoLink: ['', [Validators.required]],
-    creationDate: ['', [Validators.required]],
+    creationDate: ['', [Validators.required, dateValidator]],
     tags: this.fb.array([this.fb.control('', Validators.required)]),
   });
 
@@ -34,5 +35,37 @@ export class CreateItemComponent {
     if (this.tags.length < 5) {
       this.tags.push(this.fb.control('', Validators.required));
     }
+  }
+
+  getTitleErrorMessage(): string {
+    const titleControl = this.createItemForm.get('title');
+
+    if (titleControl?.hasError('required')) {
+      return 'Please enter a title';
+    }
+
+    if (titleControl?.hasError('minlength')) {
+      return 'The title is too short';
+    }
+
+    if (titleControl?.hasError('maxlength')) {
+      return 'The title is too long';
+    }
+
+    return '';
+  }
+
+  getDateErrorMessage(): string {
+    const dateControl = this.createItemForm.get('creationDate');
+
+    if (dateControl?.hasError('required')) {
+      return 'Please enter a creation date';
+    }
+
+    if (dateControl?.hasError('futureDate')) {
+      return 'The date is invalid';
+    }
+
+    return '';
   }
 }
