fcb73cd91d8491f7920160cad5c5b805eae445a1
Denis Ivanchenko <sicknature24@yandex.com>
2024-07-22 12:50:00 +0300

    feat: implement reactive form for create-item component

--- youtube-client-app/src/app/youtube/pages/create-item/create-item.component.ts
+++ youtube-client-app/src/app/youtube/pages/create-item/create-item.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-item',
   templateUrl: './create-item.component.html',
   styleUrls: ['./create-item.component.scss'],
 })
-export class CreateItemComponent {}
+export class CreateItemComponent {
+  createItemForm = this.fb.group({
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    imageLink: ['', [Validators.required]],
+    videoLink: ['', [Validators.required]],
+    creationDate: ['', [Validators.required]],
+    tags: this.fb.array([this.fb.control('', Validators.required)]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get tags(): FormArray {
+    return this.createItemForm.get('tags') as FormArray;
+  }
+
+  onFormReset(): void {
+    this.createItemForm.reset();
+    this.tags.clear();
+    this.tags.push(this.fb.control('', Validators.required));
+  }
+
+  onFormSubmit(): void {}
+
+  onAddTag(): void {
+    if (this.tags.length < 5) {
+      this.tags.push(this.fb.control('', Validators.required));
+    }
+  }
+}
